583b993b04f0c7deef4a4d684538f9e5a31307e6
Damian <jdamianlh@gmail.com>
2024-08-13 00:28:51 -0500

    Agregando NavLink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {  NavLink, useLocation } from 'react-router-dom'
 
 export default function Header() {
+
+    const {pathname} = useLocation()
+
+    console.log(pathname);
+    
 
   return (
     <header className='bg-slate-800'>
@@ -10,8 +16,17 @@ export default function Header() {
                     <img className='w-32' src="/logo.svg" alt="logotipo" />
                 </div>
 
-                <nav>
-
+                <nav className='flex gap-4'>
+                    <NavLink 
+                        to="/"
+                        className={({isActive}) => 
+                        isActive ? 'text-orange-500 font-bold uppercase' : 'text-white font-bold uppercase'}
+                    >Inicio</NavLink>
+                    <NavLink 
+                        to="/favoritos"
+                        className={({isActive}) => 
+                        isActive ? 'text-orange-500 font-bold uppercase' : 'text-white font-bold uppercase'}
+                    >Favoritos</NavLink>
                 </nav>
             </div>
         </div>
